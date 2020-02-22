d83fb7fe0831123986a84a317218d939936d7713
Asabeneh <asabeneh@gmail.com>
2020-02-22 17:17:37 +0200

    react-boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './assets/styles/styles.scss'
 import './App.css'
+import reactLogo from './assets/images/react_logo.png'
 
 function App() {
   return (
     <div className='App'>
       <h1>React App</h1>
+      <img src={reactLogo} alt='react logo' />
     </div>
   )
 }
